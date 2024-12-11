b1b1eaf8511e43c1488f11bb9864210f2f904f1a
Jon Openshaw <10333387+jonopens@users.noreply.github.com>
2024-12-10 22:51:56 -0500

    removes unused useEffect import

--- src/hooks/useLocks.ts
+++ src/hooks/useLocks.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { IUnlockContext, UnlockContext } from '../context/UnlockContext';
 
 const confirmMessages: string[] = [
